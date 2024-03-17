6a68d3440f1c65aa29470aaeba76f38045d02485
SrijaC2 <srijachanda06@gmail.com>
2024-03-17 17:07:03 +0530

    smarter-tasks

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+import { RouterProvider } from "react-router-dom";
+import "./App.css";
+import router from "./routes";
+import { ThemeContext } from "./context/theme";
+import { ProjectsProvider } from "./context/projects/context";
+import { UsersProvider } from "./context/members/context";
+
+const App = () => {
+  const { theme } = useContext(ThemeContext);
+  return (
+    <div
+      className={`h-screen w-full mx-auto py-2 ${
+        theme === "dark" ? "dark" : ""
+      }`}
+    >
+      <ProjectsProvider>
+        <UsersProvider>
+          <RouterProvider router={router} />
+        </UsersProvider>
+      </ProjectsProvider>
+    </div>
+  );
+};
+export default App;
---
+++ src/pages/members/index.tsx
@@ -0,0 +1,23 @@
+import React, { Suspense } from "react";
+const MemberList = React.lazy(() => import("./MemberList"));
+import NewMember from "./NewMember";
+import ErrorBoundary from "../../components/ErrorBoundary";
+
+const Members = () => {
+  return (
+    <>
+      <div className="flex justify-between">
+        <h2 className="text-2xl font-medium tracking-tight">Members</h2>
+        <NewMember />
+      </div>
+      <ErrorBoundary>
+        <Suspense fallback={<div className="suspense-loading">Loading...</div>}>
+        <MemberList />
+        </Suspense>
+      </ErrorBoundary>
+      
+    </>
+  );
+};
+
+export default Members;
